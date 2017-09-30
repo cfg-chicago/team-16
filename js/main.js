612b35f62487de5107280143fe61419c764afd69
Sean Howard <snhoward@me.com>
2017-09-30 01:51:27 -0500

    commented out old code

--- js/main.js
+++ js/main.js
@@ -3,19 +3,19 @@ console.log("start");
 // Get a reference to the database service
 var database = firebase.database();
 
-//Test data
-var userIdx = "lj23";
-var namex = "Lebron James";
-var emailx = "lebron.james23@gmail.com";
-var imageUrlx = "google.com";
-
-function writeTestData(userId, name, email, imageUrl) {
-  database.ref('tests/' + userId).set({
-    username: name,
-    email: email,
-    profile_picture : imageUrl
-  });
-}
+// //Test data
+// var userIdx = "lj23";
+// var namex = "Lebron James";
+// var emailx = "lebron.james23@gmail.com";
+// var imageUrlx = "google.com";
+//
+// function writeTestData(userId, name, email, imageUrl) {
+//   database.ref('tests/' + userId).set({
+//     username: name,
+//     email: email,
+//     profile_picture : imageUrl
+//   });
+// }
 //writeTestData(userIdx, namex, emailx, imageUrlx);
 
 /**
@@ -71,131 +71,128 @@ function getJourneyPageInfo(journeyId) {
 
 function getProfilePageInfo(userId) {
   var profile_name = database.ref().child('users').child(userId).child('name');
-  var journey_description = database.ref().child('journeys').child(journeyId).child('description');
-  var ratings = database.ref().child('journeys').child(journeyId).child('ratings');
+  var posts = database.ref().child('users').child(userId).child('posts');
+  var profile_picture = database.ref().child('users').child(userId).child('profile_picture');
+  var journeys = database.ref().child('users').child(userId).child('journeys');
 }
 
-function getPosts() {
-
+function getPosts(posts) {
+  console.log(posts);
 }
 
 
-
-
-
-
-/**
- * Attempts to validate an account
- */
-function attemptSignIn() {
-    var message = document.getElementById("message-to-sign-in");
-    message.innerHTML = "<img class='loading' src='img/loading.gif'></img>";
-    var msgbox = document.getElementById("sign-in-box");
-    var usernameInput = document.getElementById("username-sign-in-input");
-    var usernameStorage = usernameInput.value;
-    var passwordInput = document.getElementById("password-sign-in-input");
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            if (this.responseText == "valid") {
-                document.getElementById("navbars").innerHTML = "&#9776; " + shorten(usernameStorage, 10);
-                ACTIVE_UID = usernameStorage;
-                usernameInput.value = '';
-                passwordInput.value = '';
-                msgbox.style.display = "none";
-                document.getElementById("create-conversation-link").style.display = "block";
-                toggleSignOutCreateLinks(true);
-                updateMessageList();
-                checkForEnableSubmit();
-            } else {
-                passwordInput.value = '';
-                message.innerHTML = "Invalid account details!";
-                checkForSignInSubmit();
-                toggleSignOutCreateLinks(false);
-            }
-        }
-    };
-    xmlhttp.open("GET", "authenticateAccountHandler.php?u=" + usernameStorage + "&p=" + passwordInput.value, true);
-    xmlhttp.send();
-}
-
-/**
- * Attempts to create an account
- */
-function attemptCreate() {
-    var message = document.getElementById("message-to-create-in");
-    message.innerHTML = "<img class='loading' src='img/loading.gif'></img>";
-    var msgbox = document.getElementById("create-in-box");
-    var usernameInput = document.getElementById("username-create-in-input");
-    var usernameStorage = usernameInput.value;
-    var passwordInput = document.getElementById("password-create-in-input");
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            if (this.responseText == "created") {
-                closeNav();
-                document.getElementById("navbars").innerHTML = "&#9776; " + shorten(usernameStorage, 10);
-                ACTIVE_UID = usernameStorage;
-                usernameInput.value = '';
-                passwordInput.value = '';
-                msgbox.style.display = "none";
-                updateUserList();
-                document.getElementById("create-conversation-link").style.display = "block";
-                toggleSignOutCreateLinks(true);
-                updateMessageList();
-                checkForEnableSubmit();
-            } else {
-                passwordInput.value = '';
-                message.innerHTML = this.responseText;
-                checkForCreateAccountSubmit();
-                toggleSignOutCreateLinks(false);
-            }
-        }
-    };
-    xmlhttp.open("GET", "createAccountHandler.php?u=" + usernameInput.value + "&p=" + passwordInput.value, true);
-    xmlhttp.send();
-}
-
-/**
- * Attempts to create a message
- */
-function attemptCreateMessage() {
-    document.getElementById("message-input-button").disabled = true;
-    var message = document.getElementById("message-input");
-    var content = message.value;
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            if (this.responseText == "created") {
-                message.value = "";
-                updateMessageList();
-            } else {
-                showErrorMessage(this.responseText);
-            }
-            checkForEnableSubmit();
-        }
-    };
-    var conversation_id = document.getElementById(ACTIVE_CONVERSATION_ID).id;
-    xmlhttp.open("GET", "createMessageHandler.php?c=" + content + "&u=" + ACTIVE_UID + "&i=" + conversation_id, true);
-    xmlhttp.send();
-
-}
-
-/**
- * Display an error message to the user
- */
-function showErrorMessage(message) {
-    var msgbox = document.getElementById("error-message-box");
-    var msgcontent = document.getElementById("message-to-error-out");
-    message = "Sorry about that. We had an error.<br><br>" + message;
-    var time = message.length * 20 + 300;
-    msgcontent.innerHTML = message;
-    msgbox.style.display = "block";
-    window.setTimeout(function () {
-        msgbox.style.display = "none";
-        msgcontent.innerHTML = "";
-    }, time);
-}
+// /**
+//  * Attempts to validate an account
+//  */
+// function attemptSignIn() {
+//     var message = document.getElementById("message-to-sign-in");
+//     message.innerHTML = "<img class='loading' src='img/loading.gif'></img>";
+//     var msgbox = document.getElementById("sign-in-box");
+//     var usernameInput = document.getElementById("username-sign-in-input");
+//     var usernameStorage = usernameInput.value;
+//     var passwordInput = document.getElementById("password-sign-in-input");
+//     var xmlhttp = new XMLHttpRequest();
+//     xmlhttp.onreadystatechange = function () {
+//         if (this.readyState == 4 && this.status == 200) {
+//             if (this.responseText == "valid") {
+//                 document.getElementById("navbars").innerHTML = "&#9776; " + shorten(usernameStorage, 10);
+//                 ACTIVE_UID = usernameStorage;
+//                 usernameInput.value = '';
+//                 passwordInput.value = '';
+//                 msgbox.style.display = "none";
+//                 document.getElementById("create-conversation-link").style.display = "block";
+//                 toggleSignOutCreateLinks(true);
+//                 updateMessageList();
+//                 checkForEnableSubmit();
+//             } else {
+//                 passwordInput.value = '';
+//                 message.innerHTML = "Invalid account details!";
+//                 checkForSignInSubmit();
+//                 toggleSignOutCreateLinks(false);
+//             }
+//         }
+//     };
+//     xmlhttp.open("GET", "authenticateAccountHandler.php?u=" + usernameStorage + "&p=" + passwordInput.value, true);
+//     xmlhttp.send();
+// }
+//
+// /**
+//  * Attempts to create an account
+//  */
+// function attemptCreate() {
+//     var message = document.getElementById("message-to-create-in");
+//     message.innerHTML = "<img class='loading' src='img/loading.gif'></img>";
+//     var msgbox = document.getElementById("create-in-box");
+//     var usernameInput = document.getElementById("username-create-in-input");
+//     var usernameStorage = usernameInput.value;
+//     var passwordInput = document.getElementById("password-create-in-input");
+//     var xmlhttp = new XMLHttpRequest();
+//     xmlhttp.onreadystatechange = function () {
+//         if (this.readyState == 4 && this.status == 200) {
+//             if (this.responseText == "created") {
+//                 closeNav();
+//                 document.getElementById("navbars").innerHTML = "&#9776; " + shorten(usernameStorage, 10);
+//                 ACTIVE_UID = usernameStorage;
+//                 usernameInput.value = '';
+//                 passwordInput.value = '';
+//                 msgbox.style.display = "none";
+//                 updateUserList();
+//                 document.getElementById("create-conversation-link").style.display = "block";
+//                 toggleSignOutCreateLinks(true);
+//                 updateMessageList();
+//                 checkForEnableSubmit();
+//             } else {
+//                 passwordInput.value = '';
+//                 message.innerHTML = this.responseText;
+//                 checkForCreateAccountSubmit();
+//                 toggleSignOutCreateLinks(false);
+//             }
+//         }
+//     };
+//     xmlhttp.open("GET", "createAccountHandler.php?u=" + usernameInput.value + "&p=" + passwordInput.value, true);
+//     xmlhttp.send();
+// }
+//
+// /**
+//  * Attempts to create a message
+//  */
+// function attemptCreateMessage() {
+//     document.getElementById("message-input-button").disabled = true;
+//     var message = document.getElementById("message-input");
+//     var content = message.value;
+//     var xmlhttp = new XMLHttpRequest();
+//     xmlhttp.onreadystatechange = function () {
+//         if (this.readyState == 4 && this.status == 200) {
+//             if (this.responseText == "created") {
+//                 message.value = "";
+//                 updateMessageList();
+//             } else {
+//                 showErrorMessage(this.responseText);
+//             }
+//             checkForEnableSubmit();
+//         }
+//     };
+//     var conversation_id = document.getElementById(ACTIVE_CONVERSATION_ID).id;
+//     xmlhttp.open("GET", "createMessageHandler.php?c=" + content + "&u=" + ACTIVE_UID + "&i=" + conversation_id, true);
+//     xmlhttp.send();
+//
+// }
+//
+// /**
+//  * Display an error message to the user
+//  */
+// function showErrorMessage(message) {
+//     var msgbox = document.getElementById("error-message-box");
+//     var msgcontent = document.getElementById("message-to-error-out");
+//     message = "Sorry about that. We had an error.<br><br>" + message;
+//     var time = message.length * 20 + 300;
+//     msgcontent.innerHTML = message;
+//     msgbox.style.display = "block";
+//     window.setTimeout(function () {
+//         msgbox.style.display = "none";
+//         msgcontent.innerHTML = "";
+//     }, time);
+// }
 
 
 console.log("finish");
