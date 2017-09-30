eaff873181beefa7839c00b02862f5a6e39ced68
David Garry <dgarry@usc.edu>
2017-09-30 00:54:47 -0500

    dg

--- Login/js/main.js
+++ Login/js/main.js
@@ -2,4 +2,7 @@ $('.message a').click(function(){
    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
 });
 
-
+$("#loginbutton").click(function(){
+  var location = "../html/index.html?usn=" + $("#loginbutton").val;
+  window.location.href = location;
+});
