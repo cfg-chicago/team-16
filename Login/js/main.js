591815efbbe08dee7b71187b51c43aaa016e300b
David Garry <dgarry@usc.edu>
2017-09-30 01:10:53 -0500

    add

--- Login/js/main.js
+++ Login/js/main.js
@@ -3,6 +3,19 @@ $('.message a').click(function(){
 });
 
 $("#loginbutton").click(function(){
-  var location = "../html/index.html?usn=" + $("#loginbutton").val;
+  var usn = document.getElementById("loginusername").value;
+  var pw = document.getElementById("loginpassword").value;
+  var location = "../html/index.html?usn=" + usn + "?hash=" + pw.hashCode();
   window.location.href = location;
 });
+
+String.prototype.hashCode = function() {
+  var hash = 0, i, chr;
+  if (this.length === 0) return hash;
+  for (i = 0; i < this.length; i++) {
+    chr   = this.charCodeAt(i);
+    hash  = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+};
